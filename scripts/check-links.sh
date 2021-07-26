cce8ad04c398b49c45a41a5e1590e403323ec2ab
Yash Totale <totaleyash@gmail.com>
2021-07-26 11:33:34 -0700

    Updated Check Links Script

--- scripts/check-links.sh
+++ scripts/check-links.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-find . -name "*.md" | grep -v node_modules | while read -r line; do
+find . -name "*.md" | grep -v node_modules | grep -v README_OLD.md | while read -r line; do
   linkinator $line --markdown --config "$(pwd)/.linkinatorrc"
 done
