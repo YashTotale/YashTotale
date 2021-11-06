fa2e2e0891ef8f709f7907883670178674d13ee3
Yash Totale <totaleyash@gmail.com>
2021-11-06 15:11:27 -0700

    Fix Puppeteer

--- scripts/generate/services/puppeteer.ts
+++ scripts/generate/services/puppeteer.ts
@@ -1,29 +1,28 @@
 // Externals
 import Logger from "@hack4impact/logger";
-import puppeteer from "puppeteer";
+import vanillaPuppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
 // Internals
 import { retry } from "./helpers";
 
 class PuppeteerService {
-  browser: puppeteer.Browser;
-  page: puppeteer.Page;
+  browser: vanillaPuppeteer.Browser;
+  page: vanillaPuppeteer.Page;
 
   async init() {
     Logger.log("Initializing puppeteer...");
     this.browser = await retry(
       () =>
-        puppeteer.launch({
+        puppeteer.use(StealthPlugin()).launch({
+          headless: true,
           args: [
             "--no-sandbox",
             "--disable-setuid-sandbox",
             "--disable-gpu",
             "--disable-infobars",
             "--window-position=0,0",
-            "--ignore-certifcate-errors",
-            "--ignore-certifcate-errors-spki-list",
-            "--incognito",
-            "--proxy-server=http=194.67.37.90:3128",
           ],
         }),
       3
@@ -52,25 +51,33 @@ class PuppeteerService {
   }
 
   async getLatestInstagramPostsFromAccount(acc: string, n = 3) {
-    Logger.log(`Getting ${n} images for '@${acc}'...`);
-    const page = `https://www.picuki.com/profile/${acc}`;
+    Logger.log(`Navigating to '@${acc}' Instagram page...`);
+
+    const page = `https://dumpor.com/v/${acc}`;
+    const selector = ".content__img";
+
     await this.goToPage(page);
-
+    await this.page.waitForSelector(selector);
     await this.page.evaluate("window.scrollTo(0, document.body.scrollHeight)");
-    await this.page.waitForTimeout(1000);
+    Logger.success(`Navigated to '@${acc}' Instagram page!`);
 
-    const images = await this.page.evaluate<(n: number) => string[]>((n) => {
-      const nodes = document.querySelectorAll<HTMLImageElement>(".post-image");
-      const images = [];
+    Logger.log(`Getting ${n} images for '@${acc}'...`);
+    const images = await this.page.evaluate(
+      (n, selector) => {
+        const nodes = document.querySelectorAll<HTMLImageElement>(selector);
+        const images = [];
 
-      for (let i = 0; i < n; i++) {
-        images.push(nodes[i].src);
-      }
+        for (let i = 0; i < n; i++) {
+          images.push(nodes[i].src);
+        }
 
-      return images;
-    }, n);
-
+        return images;
+      },
+      n,
+      selector
+    );
     Logger.success(`Got ${n} images for '@${acc}'!`);
+
     return images;
   }
 }
